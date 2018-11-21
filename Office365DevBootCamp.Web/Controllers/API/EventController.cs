918163908b5abee0a4ff5bb09403660a8123287e
Dara Oladapo <daraoladapo@outlook.com>
2018-11-21 15:54:16 +0100

    moving coding to PC

--- Office365DevBootCamp.Web/Controllers/API/EventController.cs
+++ Office365DevBootCamp.Web/Controllers/API/EventController.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AdaptiveCards;
+using AdaptiveCards.Rendering.Html;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Office365DevBootCamp.Web.Data;
 using Office365DevBootCamp.Web.Models;
 using Office365DevBootCamp.Web.Models.Binding;
 using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Threading.Tasks;
 
@@ -21,6 +24,7 @@ namespace Office365DevBootCamp.Web.Controllers.API
         [Route("register")]
         public async Task<IActionResult> Register([FromBody] RegistrationBindingModel registrationBindingModel)
         {
+            HtmlTag htmlTag = new HtmlTag("", true);
             if (string.IsNullOrWhiteSpace(registrationBindingModel.EmailAddress))
             {
                 return BadRequest(new GenericViewModel
@@ -51,9 +55,50 @@ namespace Office365DevBootCamp.Web.Controllers.API
                 EmailAddress = registrationBindingModel.EmailAddress,
                 RegistrationDate = DateTime.Now
             };
+            // Create a card renderer
+            AdaptiveCardRenderer renderer = new AdaptiveCardRenderer();
+            // For fun, check the schema version this renderer supports
+            AdaptiveSchemaVersion schemaVersion = renderer.SupportedSchemaVersion; // 1.0
+
+            // Build a simple card
+            // In the real world this would probably be provided as JSON
+            var Actions = new List<AdaptiveAction>();
+            Actions.Add(
+                item: 
+            );
+            AdaptiveCard card = new AdaptiveCard()
+            {
+                Body = new List<AdaptiveElement>(){
+new AdaptiveContainer(){},
+new AdaptiveContainer(){}
+                },
+                Actions = new List<AdaptiveAction>()
+                {
+
+                }
+            };
+
+            try
+            {
+                // Render the card
+                RenderedAdaptiveCard renderedCard = renderer.RenderCard(card);
+
+                // Get the output HTML 
+                htmlTag = renderedCard.Html;
+
+                // (Optional) Check for any renderer warnings
+                // This includes things like an unknown element type found in the card
+                // Or the card exceeded the maxmimum number of supported actions, etc
+                IList<AdaptiveWarning> warnings = renderedCard.Warnings;
+            }
+            catch (AdaptiveException ex)
+            {
+                // Failed rendering
+            }
+
             var _Attendee = _context.Attendees.Add(attendee);
             await _context.SaveChangesAsync();
-            await Services.EmailService.SendRegistrationEmail(attendee);
+            await Services.EmailService.SendRegistrationEmail(attendee, htmlTag);
 
             return Created($"http://events.windforcecorp.com/{_Attendee.Entity.AttendeeID.ToString()}", _Attendee);
         }
--- Office365DevBootCamp.Web/Models/Data/Attendee.cs
+++ Office365DevBootCamp.Web/Models/Data/Attendee.cs
@@ -12,12 +12,14 @@ namespace Office365DevBootCamp.Web.Models.Data
         public Guid AttendeeID { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string  FullName { get {return $"{FirstName} {LastName}";} }
+        public string FullName { get { return $"{FirstName} {LastName}"; } }
         public string EmailAddress { get; set; }
         public string PhoneNumber { get; set; }
         public DateTime RegistrationDate { get; set; }
         public bool IsConfirmed { get; set; }
         public DateTime ConfirmationDate { get; set; }
+        public bool IsCanlled { get; set; }
+        public DateTime CancellationDate { get; set; }
         public int EventRating { get; set; }
     }
 }
